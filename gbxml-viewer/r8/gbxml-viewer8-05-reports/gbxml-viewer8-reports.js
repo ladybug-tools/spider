e231e9407b4a49ae358318b272a2e80f2d0143ce
Theo Armour <theo-armour@users.noreply.github.com>
2017-12-01 16:03:40 -0800

    more let to var

--- gbxml-viewer/r8/gbxml-viewer8-05-reports/gbxml-viewer8-reports.js
+++ gbxml-viewer/r8/gbxml-viewer8-05-reports/gbxml-viewer8-reports.js
@@ -1,8 +1,6 @@
 // Copyright 2017 Ladybug Tools authors. MIT License
 
-
 	var divAppMenu = divAppMenu || undefined;
-
 
 	var icw;
 	var gbjson;
@@ -35,7 +33,7 @@ console.log( 'gbjson', icw.gbjson );
 		let txt = 'lorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?';
 
 		divAppMenu.innerHTML = 
-//			txt +
+
 			'<div id = divReport ></div>';
 
 
