ccc014e3ed4892f72f9e9d050cc073d8278bbca2
Theo Armour <theo-armour@users.noreply.github.com>
2018-06-02 15:50:39 -0700

    gvb r3

--- gbxml-viewer-basic/README.md
+++ gbxml-viewer-basic/README.md
@@ -1,12 +1,12 @@
 <span style=display:none; >[You are now in a GitHub source code view - click this link to view Read Me file as a web page](http://www.ladybug.tools/spider/index.html#gbxml-viewer-basic/README.md "View file as a web page." ) </span>
 
-<input type=button onclick="window.location.href='https://github.com/ladybug-tools/spider/blob/master/gbxml-viewer-basic/README.md'";
-value='You are now in a GitHub web page view - Click this button to view this read me file as source code' >
+<div><input type=button onclick="window.location.href='https://github.com/ladybug-tools/spider/blob/master/gbxml-viewer-basic/README.md'";
+value='You are now in a GitHub web page view - Click this button to view this read me file as source code' ></div>
 
 # [gbXML Viewer Basic Read Me]( #gbxml-viewer-basic/README.md )
 
 
-<iframe class=iframeReadMe src=http://www.ladybug.tools/spider/gbxml-viewer-basic/r2/gbxml-viewer-basic.html width=100% height=400px >Iframes are not displayed on github.com</iframe>
+<iframe class=iframeReadMe src=http://www.ladybug.tools/spider/gbxml-viewer-basic/r3/gbxml-viewer-basic.html width=100% height=400px >Iframes are not displayed on github.com</iframe>
 
 
 ## Latest stable release: [gbXML Viewer Basic]( http://www.ladybug.tools/spider/gbxml-viewer-basic/index.html )
@@ -14,15 +14,21 @@ value='You are now in a GitHub web page view - Click this button to view this re
 
 ## Concept
 
-A very basic gbXML file viewer
+The ['Aragog' gbXML Viewer]( http://www.ladybug.tools/spider/gbxml-viewer/r13/gv-app-application/gv-app.html ) is a nice bit of work - or it will be one day - but the code base is getting lengthy and convoluted. It's no longer a place for beginners to start. You can't just open it up and see what's going on in fifteen minutes or so.
+
+Therefore we now have gbXML Viewer Basic
+
+This is a very basic gbXML file viewer
 * Use this code if you want a the basic idea of how to read a gbXML file and turn it into a Three.js scene
 * Use this code if you want to learn how to take sets of coplanar points and convert these to Three.js 2D Shapes with holes position and rotated arbitrarily in 3D space.
 
 The intention is that the core gbXML parser here and the core parser in the full-featured gbXML Viewer and the parser in the RAD Viewer all contain identical or nearly identical code.
 
-The code should be simple, fast and easy to read. If you can read and understand the Three.js examples, then you should feel at home here.
+The code should be simple, fast and easy to read. If you can read and understand the Three.js examples and the [Mr.doob coding style&trade; ]( https://github.com/mrdoob/three.js/wiki/Mr.doob's-Code-Style%E2%84%A2 ), then you should feel at home here. Be prepared to enjoy the white space!
 
+A good function to look at is ```GBX.get3dShape()```. This function takes a set of 3D coplanar points along with points that bound any holes and converts these into a Three.js mesh bounded by those points. This mesh is created using a Three.js Shape - which is excellent stuff but very 2D. The conversion requires that you conjugate your quaternions and do all manner of other unreal linear algebra thinking. All this in just 300 or so lines of code.
 
+The gbXML portions are also interesting. The routines end up with three sets of data for every element in the original file: XML, JSON and Three.js meshes.
 
 
 ## Wish list
@@ -31,6 +37,7 @@ The code should be simple, fast and easy to read. If you can read and understand
 
 ## Issues
 
+2018-06-02 ~ Theo ~ currently surfaces, edges and opening are each their oen group of objects. Could nicer things happen if the edges and opening were children of their parent surface??
 
 
 ## Links of Interest
@@ -40,6 +47,17 @@ The code should be simple, fast and easy to read. If you can read and understand
 
 
 ## Change Log
+
+### 2018-06-02 ~ Theo
+
+R3
+* Much improved geometry dispose
+* Memory information display
+* Two more files to load on menu
+* Add visibility toggles
+* Add zoom all button
+* More cleanup and updating variable names
+
 
 ### 2018-06-01 ~ Theo
 
--- gbxml-viewer-basic/r2/gbxml-viewer-basic.js
+++ gbxml-viewer-basic/r2/gbxml-viewer-basic.js
@@ -2,329 +2,329 @@
 /* globals THREE */
 /* jshint esversion: 6 */
 
-	var GBX = {};
+var GBX = {};
 
-	GBX.gbxml = null;
-	GBX.gbjson = null; // xnl converted to json
-	GBX.surfaceJson = null; // useful subset of GBX.gbjson
+GBX.gbxml = null;
+GBX.gbjson = null; // xnl converted to json
+GBX.surfaceJson = null; // useful subset of GBX.gbjson
 
-	GBX.surfaceMeshes= null; // Three.js Shapes as Meshes created from GBX.surfaceJson
-	GBX.surfaceEdges= null; // Three.js edges helper creare from GBX.surfaceMeshes
-	GBX.surfaceOpenings= null; // Three.js Three.js Shapes as Meshes created created from GBX.surfaceJson with Openings
+GBX.surfaceMeshes= null; // Three.js Shapes as Meshes created from GBX.surfaceJson
+GBX.surfaceEdges= null; // Three.js edges helper creare from GBX.surfaceMeshes
+GBX.surfaceOpenings= null; // Three.js Three.js Shapes as Meshes created created from GBX.surfaceJson with Openings
 
-	GBX.colorsDefault = {
+GBX.colorsDefault = {
 
-		InteriorWall: 0x008000,
-		ExteriorWall: 0xFFB400,
-		Roof: 0x800000,
-		InteriorFloor: 0x80FFFF,
-		ExposedFloor: 0x40B4FF,
-		Shade: 0xFFCE9D,
-		UndergroundWall: 0xA55200,
-		UndergroundSlab: 0x804000,
-		Ceiling: 0xFF8080,
-		Air: 0xFFFF00,
-		UndergroundCeiling: 0x408080,
-		RaisedFloor: 0x4B417D,
-		SlabOnGrade: 0x804000,
-		FreestandingColumn: 0x808080,
-		EmbeddedColumn: 0x80806E
+	InteriorWall: 0x008000,
+	ExteriorWall: 0xFFB400,
+	Roof: 0x800000,
+	InteriorFloor: 0x80FFFF,
+	ExposedFloor: 0x40B4FF,
+	Shade: 0xFFCE9D,
+	UndergroundWall: 0xA55200,
+	UndergroundSlab: 0x804000,
+	Ceiling: 0xFF8080,
+	Air: 0xFFFF00,
+	UndergroundCeiling: 0x408080,
+	RaisedFloor: 0x4B417D,
+	SlabOnGrade: 0x804000,
+	FreestandingColumn: 0x808080,
+	EmbeddedColumn: 0x80806E
 
-	};
+};
 
-	GBX.colors = Object.assign( {}, GBX.colorsDefault ); // working copy of default colors
+GBX.colors = Object.assign( {}, GBX.colorsDefault ); // working copy of default colors
 
-	GBX.surfaceTypes  = Object.keys( GBX.colors );
-
-
-	// loads any xml file - from AJAX, file reader or location hash or wherever
-
-	GBX.parseFileXML = function( xml ) {
-
-		GBX.gbxml = xml;
-
-		GBX.gbjson = GBX.getXML2jsobj( GBX.gbxml );
-		console.log( 'GBX.gbjson', GBX.gbjson );
-
-		GBX.surfacesJson = GBX.gbjson.Campus.Surface;
-
-		GBX.surfaceEdges = new THREE.Object3D();
-		GBX.surfaceEdges.name = 'GBX.surfaceEdges';
-
-		GBX.surfaceMeshes = GBX.getSurfaceMeshes( GBX.gbjson );
-
-		GBX.surfaceOpenings  = GBX.getOpenings();
-
-		return [ GBX.surfaceMeshes, GBX.surfaceEdges, GBX.surfaceOpenings ];
-
-	};
+GBX.surfaceTypes  = Object.keys( GBX.colors );
 
 
-	// https://www.sitepoint.com/how-to-convert-xml-to-a-javascript-object/
-	// http://blogs.sitepointstatic.com/examples/tech/xml2json/index.html
-	// Theo: I have difficulty understanding how this functions
+// loads any xml file - from AJAX, file reader or location hash or wherever
 
-	GBX.getXML2jsobj = function( node ) {
+GBX.parseFileXML = function( xml ) {
 
-		let data = {};
+	GBX.gbxml = xml;
 
-		function Add( name, value ) {
+	GBX.gbjson = GBX.getXML2jsobj( GBX.gbxml );
+	console.log( 'GBX.gbjson', GBX.gbjson );
 
-			if ( data[ name ] ) {
+	GBX.surfacesJson = GBX.gbjson.Campus.Surface;
 
-				if ( data[ name ].constructor !== Array ) {
+	GBX.surfaceEdges = new THREE.Object3D();
+	GBX.surfaceEdges.name = 'GBX.surfaceEdges';
 
-					data[ name ] = [ data[ name ] ];
+	GBX.surfaceMeshes = GBX.getSurfaceMeshes( GBX.gbjson );
 
-				}
+	GBX.surfaceOpenings  = GBX.getOpenings();
 
-				data[ name ][ data[ name ].length ] = value;
+	return [ GBX.surfaceMeshes, GBX.surfaceEdges, GBX.surfaceOpenings ];
 
-			} else {
+};
 
-				data[ name ] = value;
+
+// https://www.sitepoint.com/how-to-convert-xml-to-a-javascript-object/
+// http://blogs.sitepointstatic.com/examples/tech/xml2json/index.html
+// Theo: I have difficulty understanding how this functions
+
+GBX.getXML2jsobj = function( node ) {
+
+	let data = {};
+
+	function Add( name, value ) {
+
+		if ( data[ name ] ) {
+
+			if ( data[ name ].constructor !== Array ) {
+
+				data[ name ] = [ data[ name ] ];
+
+			}
+
+			data[ name ][ data[ name ].length ] = value;
+
+		} else {
+
+			data[ name ] = value;
+
+		}
+
+	}
+
+	let child, childNode;
+
+	for ( child = 0; childNode = node.attributes[ child ]; child++ ) {
+
+		Add( childNode.name, childNode.value );
+
+	}
+
+	for ( child = 0; childNode = node.childNodes[ child ]; child++ ) {
+
+		if ( childNode.nodeType === 1 ) {
+
+			if ( childNode.childNodes.length === 1 && childNode.firstChild.nodeType === 3 ) { // text value
+
+				Add( childNode.nodeName, childNode.firstChild.nodeValue );
+
+			} else { // sub-object
+
+				Add( childNode.nodeName, GBX.getXML2jsobj( childNode ) );
 
 			}
 
 		}
 
-		let child, childNode;
+	}
 
-		for ( child = 0; childNode = node.attributes[ child ]; child++ ) {
+	return data;
 
-			Add( childNode.name, childNode.value );
-
-		}
-
-		for ( child = 0; childNode = node.childNodes[ child ]; child++ ) {
-
-			if ( childNode.nodeType === 1 ) {
-
-				if ( childNode.childNodes.length === 1 && childNode.firstChild.nodeType === 3 ) { // text value
-
-					Add( childNode.nodeName, childNode.firstChild.nodeValue );
-
-				} else { // sub-object
-
-					Add( childNode.nodeName, GBX.getXML2jsobj( childNode ) );
-
-				}
-
-			}
-
-		}
-
-		return data;
-
-	};
+};
 
 
 
-	GBX.getSurfaceMeshes = function() {
+GBX.getSurfaceMeshes = function() {
 
-		const surfaces = GBX.surfacesJson; // gbjson.Campus.Surface;
-		const holeVertices = [];
-		const shapeVertices = [];
+	const surfaces = GBX.surfacesJson; // gbjson.Campus.Surface;
+	const holeVertices = [];
+	const shapeVertices = [];
 
-		for ( let surface of surfaces ) {
+	for ( let surface of surfaces ) {
 
-			const verticesGroup = [];
+		const verticesGroup = [];
 
-			if ( surface.Opening ) {
-
-				surface.Opening = Array.isArray( surface.Opening ) ? surface.Opening : [ surface.Opening ];
-
-				for ( let opening of surface.Opening ) {
-
-					const polyloop = opening.PlanarGeometry.PolyLoop;
-					const vertices = GBX.getPoints( polyloop );
-					verticesGroup.push( vertices );
-
-				}
-
-			}
-
-			holeVertices.push( verticesGroup );
-
-			const polyloop = surface.PlanarGeometry.PolyLoop;
-			const vertices = GBX.getPoints( polyloop );
-			shapeVertices.push( vertices );
-
-		}
-
-		let surfaceMeshes = new THREE.Object3D();
-		surfaceMeshes.name = 'GBX.surfaceMeshes';
-
-		for ( let i = 0; i < shapeVertices.length; i++ ) {
-
-			const material = new THREE.MeshPhongMaterial( {
-				color: GBX.colors[ surfaces[ i ].surfaceType ], side: 2, opacity: 0.85, transparent: true } );
-
-			const shape = GBX.getShape( shapeVertices[ i ], material, holeVertices[ i ] );
-
-			shape.userData.data = surfaces[ i ];
-			shape.castShadow = shape.receiveShadow = true;
-			surfaceMeshes.add( shape );
-
-			// split into separate function
-			const edgesGeometry = new THREE.EdgesGeometry( shape.geometry );
-			const surfaceEdge = new THREE.LineSegments( edgesGeometry, new THREE.LineBasicMaterial( { color: 0x888888 } ) );
-			surfaceEdge.rotation.copy( shape.rotation );
-			surfaceEdge.position.copy( shape.position );
-			GBX.surfaceEdges.add( surfaceEdge ); // or add to surfaces??
-
-		}
-
-		return surfaceMeshes;
-
-	};
-
-
-
-	GBX.getOpenings = function() {
-
-		const surfaceOpenings = new THREE.Object3D();
-		surfaceOpenings.name = 'GBX.surfaceOpenings';
-
-		GBX.SurfacesWithOpenings = GBX.surfacesJson.filter( surface => surface.Opening );
-
-		GBX.openingsJson = [];
-
-		for ( let surface of GBX.SurfacesWithOpenings ) {
+		if ( surface.Opening ) {
 
 			surface.Opening = Array.isArray( surface.Opening ) ? surface.Opening : [ surface.Opening ];
 
-			GBX.openingsJson.push ( ...surface.Opening );
+			for ( let opening of surface.Opening ) {
+
+				const polyloop = opening.PlanarGeometry.PolyLoop;
+				const vertices = GBX.getPoints( polyloop );
+				verticesGroup.push( vertices );
+
+			}
 
 		}
 
-		const material = new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.5, side: 2, transparent: true } );
+		holeVertices.push( verticesGroup );
 
-		for ( let opening of GBX.openingsJson ) {
+		const polyloop = surface.PlanarGeometry.PolyLoop;
+		const vertices = GBX.getPoints( polyloop );
+		shapeVertices.push( vertices );
 
-			const points = GBX.getPoints( opening.PlanarGeometry.PolyLoop );
+	}
 
-			const shapeMesh = GBX.getShape( points, material );
-			surfaceOpenings.add( shapeMesh );
+	let surfaceMeshes = new THREE.Object3D();
+	surfaceMeshes.name = 'GBX.surfaceMeshes';
 
-		}
+	for ( let i = 0; i < shapeVertices.length; i++ ) {
 
-		return surfaceOpenings;
+		const material = new THREE.MeshPhongMaterial( {
+			color: GBX.colors[ surfaces[ i ].surfaceType ], side: 2, opacity: 0.85, transparent: true } );
 
-	};
+		const shape = GBX.getShape( shapeVertices[ i ], material, holeVertices[ i ] );
 
+		shape.userData.data = surfaces[ i ];
+		shape.castShadow = shape.receiveShadow = true;
+		surfaceMeshes.add( shape );
 
+		// split into separate function
+		const edgesGeometry = new THREE.EdgesGeometry( shape.geometry );
+		const surfaceEdge = new THREE.LineSegments( edgesGeometry, new THREE.LineBasicMaterial( { color: 0x888888 } ) );
+		surfaceEdge.rotation.copy( shape.rotation );
+		surfaceEdge.position.copy( shape.position );
+		GBX.surfaceEdges.add( surfaceEdge ); // or add to surfaces??
 
-	GBX.getPoints = function( polyloop ) {
+	}
 
-		const points = polyloop.CartesianPoint.map( CartesianPoint => new THREE.Vector3().fromArray( CartesianPoint.Coordinate ) );
-		return points;
+	return surfaceMeshes;
 
-	};
-
-
-
-	GBX.getShape = function( vertices, material, holes = [] ) {
-		// split into two functions
-
-		const plane = GBX.getPlane( vertices );
-
-		const obj = new THREE.Object3D();
-		obj.lookAt( plane.normal );  // copy the rotation of the triangle
-		obj.quaternion.conjugate();
-		obj.updateMatrixWorld();
-
-		vertices.map( vertex => obj.localToWorld( vertex ) );
-
-		const shape = new THREE.Shape( vertices );
-
-		for ( let verticesHoles of holes ) {
-
-			const hole = new THREE.Path();
-
-			verticesHoles.map( vertex => obj.localToWorld( vertex ) );
-
-			hole.setFromPoints( verticesHoles );
-
-			shape.holes.push( hole );
-
-		}
-
-		const geometryShape = new THREE.ShapeGeometry( shape );
-
-		const shapeMesh = new THREE.Mesh( geometryShape, material );
-		shapeMesh.lookAt( plane.normal );
-		shapeMesh.position.copy( plane.normal.multiplyScalar( - plane.constant ) );
-
-		return shapeMesh;
-
-	};
+};
 
 
 
-	GBX.getPlane = function( points, start = 0 ) {
+GBX.getOpenings = function() {
 
-		const triangle = new THREE.Triangle();
-		triangle.set( points[ start ], points[ start + 1 ], points[ start + 2 ] );
+	const surfaceOpenings = new THREE.Object3D();
+	surfaceOpenings.name = 'GBX.surfaceOpenings';
 
-		if ( triangle.getArea() === 0 ) { // points must be colinear therefore not usable
+	GBX.SurfacesWithOpenings = GBX.surfacesJson.filter( surface => surface.Opening );
 
-			start++;
-			GBX.getPlane( points, start );
+	GBX.openingsJson = [];
 
-		}
+	for ( let surface of GBX.SurfacesWithOpenings ) {
 
-		const pl = new THREE.Plane();
-		GBX.plane = triangle.getPlane( pl ); // why global??
+		surface.Opening = Array.isArray( surface.Opening ) ? surface.Opening : [ surface.Opening ];
 
-		return GBX.plane;
+		GBX.openingsJson.push ( ...surface.Opening );
 
-	};
+	}
 
+	const material = new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.5, side: 2, transparent: true } );
 
-	//////////
+	for ( let opening of GBX.openingsJson ) {
 
-	GBX.setAllVisible = function() {
+		const points = GBX.getPoints( opening.PlanarGeometry.PolyLoop );
 
-		GBX.surfaceEdges.visible = true;
-		GBX.surfaceMeshes.visible = true;
-		GBX.surfaceOpenings.visible = true;
+		const shapeMesh = GBX.getShape( points, material );
+		surfaceOpenings.add( shapeMesh );
 
-		for ( let child of GBX.surfaceMeshes.children ) {
+	}
 
-			if ( !child.material ) { continue; }
+	return surfaceOpenings;
 
-			child.material = new THREE.MeshPhongMaterial( {
-				color: GBX.colors[ child.userData.data.surfaceType ], side: 2, opacity: 0.85, transparent: true }
-			);
-			child.material.wireframe = false;
-			child.visible = true;
-
-		}
-
-		GBX.surfaceOpenings.visible = true;
-
-		for ( let child of GBX.surfaceOpenings.children ) {
-
-			if ( !child.material ) { continue; }
-
-			child.material = new THREE.MeshPhongMaterial( {
-				color: 0x000000, side: 2, opacity: 0.5, transparent: true }
-			);
-			child.material.wireframe = false;
-			child.visible = true;
-
-		}
-
-		document.body.style.backgroundImage = '';
-
-	};
+};
 
 
-	GBX.toggleSurfacesVisible = function() {
 
-		GBX.surfaceMeshes.visible=!GBX.surfaceMeshes.visible;
+GBX.getPoints = function( polyloop ) {
 
-		GBX.surfaceMeshes.children.forEach( child => child.visible = GBX.surfaceMeshes.visible );
+	const points = polyloop.CartesianPoint.map( CartesianPoint => new THREE.Vector3().fromArray( CartesianPoint.Coordinate ) );
+	return points;
 
-	};
+};
+
+
+
+GBX.getShape = function( vertices, material, holes = [] ) {
+	// split into two functions
+
+	const plane = GBX.getPlane( vertices );
+
+	const obj = new THREE.Object3D();
+	obj.lookAt( plane.normal );  // copy the rotation of the triangle
+	obj.quaternion.conjugate();
+	obj.updateMatrixWorld();
+
+	vertices.map( vertex => obj.localToWorld( vertex ) );
+
+	const shape = new THREE.Shape( vertices );
+
+	for ( let verticesHoles of holes ) {
+
+		const hole = new THREE.Path();
+
+		verticesHoles.map( vertex => obj.localToWorld( vertex ) );
+
+		hole.setFromPoints( verticesHoles );
+
+		shape.holes.push( hole );
+
+	}
+
+	const geometryShape = new THREE.ShapeGeometry( shape );
+
+	const shapeMesh = new THREE.Mesh( geometryShape, material );
+	shapeMesh.lookAt( plane.normal );
+	shapeMesh.position.copy( plane.normal.multiplyScalar( - plane.constant ) );
+
+	return shapeMesh;
+
+};
+
+
+
+GBX.getPlane = function( points, start = 0 ) {
+
+	const triangle = new THREE.Triangle();
+	triangle.set( points[ start ], points[ start + 1 ], points[ start + 2 ] );
+
+	if ( triangle.getArea() === 0 ) { // points must be colinear therefore not usable
+
+		start++;
+		GBX.getPlane( points, start );
+
+	}
+
+	const pl = new THREE.Plane();
+	GBX.plane = triangle.getPlane( pl ); // why global??
+
+	return GBX.plane;
+
+};
+
+
+//////////
+
+GBX.setAllVisible = function() {
+
+	GBX.surfaceEdges.visible = true;
+	GBX.surfaceMeshes.visible = true;
+	GBX.surfaceOpenings.visible = true;
+
+	for ( let child of GBX.surfaceMeshes.children ) {
+
+		if ( !child.material ) { continue; }
+
+		child.material = new THREE.MeshPhongMaterial( {
+			color: GBX.colors[ child.userData.data.surfaceType ], side: 2, opacity: 0.85, transparent: true }
+		);
+		child.material.wireframe = false;
+		child.visible = true;
+
+	}
+
+	GBX.surfaceOpenings.visible = true;
+
+	for ( let child of GBX.surfaceOpenings.children ) {
+
+		if ( !child.material ) { continue; }
+
+		child.material = new THREE.MeshPhongMaterial( {
+			color: 0x000000, side: 2, opacity: 0.5, transparent: true }
+		);
+		child.material.wireframe = false;
+		child.visible = true;
+
+	}
+
+	document.body.style.backgroundImage = '';
+
+};
+
+
+GBX.toggleSurfacesVisible = function() {
+
+	GBX.surfaceMeshes.visible=!GBX.surfaceMeshes.visible;
+
+	GBX.surfaceMeshes.children.forEach( child => child.visible = GBX.surfaceMeshes.visible );
+
+};
