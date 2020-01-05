00b720582de206ed82850284e64b1d31a72fffea
Theo Armour <theo-armour@users.noreply.github.com>
2020-01-04 16:25:56 -0800

    fixes

--- sandbox/honeybee-model/sample-file-viewer/sample-file-viewer-2020-01-04-01.js
+++ sandbox/honeybee-model/sample-file-viewer/sample-file-viewer-2020-01-04-01.js
@@ -79,7 +79,7 @@ GLF.getOptions = function () {
 
 GLF.getFile = function ( index ) {
 
-	const url = GLF.prefix + GLF.filesData[ index ];
+	const url = GLF.prefix + GLF.filesData[ index ].path;
 	//const url = GLF.urls[ index ];
 	console.log( '', url );
 
@@ -92,13 +92,13 @@ GLF.getFile = function ( index ) {
 
 }
 
-GLF.listImages = function ( index ) {
+GLF.listFiles = function () {
 
-	const url = GLF.prefix + GLF.filesData[ index ];
-	console.log( '', url );
+	//const url = GLF.prefix + GLF.filesData[ index ];
+	//console.log( '', url );
 
-	files = GLF.filesData.map( url => `"${GLF.prefix }${ url }"`).join( ",<br>");
-	GLFdivImages.innerHTML = files;
+	files = GLF.filesData.map( url => `"${GLF.prefix }${ url.path }"`).join( ",<br>");
+	GLFdivFiles.innerHTML = files;
 		`
 
 `;
